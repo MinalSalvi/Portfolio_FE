d35f6134b266e1f1fa99410cece9a78c8a6aa1d2
MinalSalvi <salviminal21@gmail.com>
2023-04-24 01:13:40 +0530

    New Frot-page

--- src/app/education/education.component.ts
+++ src/app/education/education.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-education',
+
   templateUrl: './education.component.html',
   styleUrls: ['./education.component.css']
 })
